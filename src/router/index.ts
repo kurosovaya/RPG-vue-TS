c5518b59da75ef1b7936126347055513abcaa847
na.shornikov <nonickk9@gmail.com>
2024-02-11 00:25:53 +0400

    initial

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import MainGameVue from '@/components/MainGame.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -7,7 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: MainGameVue
+      component: HomeView
     },
     {
       path: '/about',
